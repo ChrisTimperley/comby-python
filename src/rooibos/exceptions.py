3c034760983b3c01c7af9a7f50cb72be79330acf
ChrisTimperley <christimperley@googlemail.com>
2018-04-25 16:40:24 -0400

    added exceptions module

---
+++ src/rooibos/exceptions.py
@@ -0,0 +1,11 @@
+class RooibosException(Exception):
+    """
+    Base class used by all Rooibos exceptions.
+    """
+
+
+class ConnectionFailure(RooibosException):
+    """
+    The client failed to establish a connection to the server within the
+    allotted connection timeout window.
+    """
