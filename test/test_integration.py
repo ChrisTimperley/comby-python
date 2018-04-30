3bcccdf42519611abe7f60d071a71ca7382d8eb1
ChrisTimperley <christimperley@googlemail.com>
2018-04-30 13:48:24 -0400

    updated integration test to use ephemeral_server method

--- test/test_integration.py
+++ test/test_integration.py
@@ -9,25 +9,8 @@ import time
 import rooibos
 
 
-@contextmanager
-def ephemeral_rooibosd(port: int = 8888) -> None:
-    url = "http://127.0.0.1:{}".format(port)
-    cmd = ["rooibosd", "-p", str(port)]
-    try:
-        process = subprocess.Popen(cmd,
-                                   # stdout=subprocess.DEVNULL,
-                                   # stderr=subprocess.DEVNULL,
-                                   preexec_fn=os.setsid)
-        yield rooibos.Client(url, timeout_connection=30)
-    finally:
-        try:
-            os.killpg(process.pid, signal.SIGTERM)
-        except UnboundLocalError:
-            pass
-
-
 def test_something():
-    with ephemeral_rooibosd() as client:
+    with rooibos.ephemeral_server() as client:
         res = client.substitute("x = :[1]", {'1': "1 + 5"})
         assert res == "x = 1 + 5"
 
