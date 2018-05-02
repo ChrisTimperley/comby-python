0006a076be42492087156d5158e101f511c911a4
ChrisTimperley <christimperley@googlemail.com>
2018-05-01 20:17:30 -0400

    added code to perform integration testing of match discovery

--- test/test_integration.py
+++ test/test_integration.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env pytest
+from typing import List, Set, Dict, Tuple, FrozenSet
 from contextlib import contextmanager
 import pytest
 import os
 import subprocess
 import signal
 import time
+import logging
 
 import rooibos
 
 
 def test_something():
+    logging.getLogger('rooibos').addHandler(logging.StreamHandler())
     with rooibos.ephemeral_server() as client:
         res = client.substitute("x = :[1]", {'1': "1 + 5"})
         assert res == "x = 1 + 5"
 
-        matches = client.matches("x = foo(bar)", "x = :[1]")
-        for match in matches:
-            print(matches)
+        def env_to_pair_set(env: rooibos.Environment) -> FrozenSet[Tuple[str, str]]:
+            return frozenset((name, env[name].fragment) for name in env)
+
+        def dict_to_pair_set(d: Dict[str, str]) -> FrozenSet[Tuple[str, str]]:
+            return frozenset((k, v) for k, v in d.items())
+
+        def check_matches(src: str, tpl: str, expected: List[Dict[str, str]]) -> None:
+            matches = list(client.matches(src, tpl))
+            actual = frozenset(env_to_pair_set(m.environment) for m in matches)
+            assert actual == frozenset(dict_to_pair_set(m) for m in expected)
+
+        check_matches("x = foo(bar)", "x = :[1]", [{'1': "foo(bar)"}])
 
 
 if __name__ == '__main__':
