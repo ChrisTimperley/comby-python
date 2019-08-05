6946569eae1970e6514df97b261c98930f2fbb92
ChrisTimperley <christimperley@googlemail.com>
2019-08-05 15:00:15 -0400

    even simpler

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-from glob import glob
-from setuptools import setup, find_packages
-import re
-import os
+import setuptools
 
-FILE_MODULE = os.path.join(os.path.dirname(__file__), 'src/comby.py')
-with open(FILE_MODULE, 'r') as f:
-    VERSION = re.search(r"__version__ = '([\d\.]+)'", f.read()).group(1)
-
-setup(version=VERSION)
+setuptools.setup()
