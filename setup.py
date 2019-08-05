1e8fea8b7e00e82b9e439b504654856accb58e27
ChrisTimperley <christimperley@googlemail.com>
2019-08-05 15:02:55 -0400

    use regex

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 import setuptools
+import re
+import os
 
-setuptools.setup()
+path = os.path.join(os.path.dirname(__file__), 'src/comby/__init__.py')
+with open(path, 'r') as f:
+    VERSION=re.search(r"__version__ = '([\d\.]+)'", f.read()).group(1)
+
+setuptools.setup(version=VERSION)
