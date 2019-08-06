03a09aa784c87c03183b67b1af37756fed3587a2
ChrisTimperley <christimperley@googlemail.com>
2019-08-06 18:13:15 -0400

    implemented call method and updated matches

--- src/comby/binary.py
+++ src/comby/binary.py
@@ -31,25 +31,55 @@ class CombyBinary(CombyInterface):
     """
     location = attr.ib(type=str, default='comby')
 
+    def call(self, args: str, text: Optional[str] = None) -> str:
+        """Calls the Comby binary.
+
+        Arguments
+        ---------
+        args: str
+            the arguments that should be supplied to the binary.
+        text: Optional[str]
+            the optional input text that should be supplied to the binary.
+
+        Returns
+        -------
+        str
+            the output of the execution.
+
+        Raises
+        ------
+        CombyBinaryError
+            if the binary produces a non-zero return code.
+        """
+        logger.debug('calling comby with args: %s', args)
+        inp = None
+        if text:
+            logger.debug('supplying input text: %s', text)
+            inp = text.encode('utf8')
+
+        cmd_s = '{} {}'.format(self.location, args)
+        p = subprocess.run(cmd_s,
+                           shell=True,
+                           stderr=subprocess.PIPE,
+                           stdout=subprocess.PIPE,
+                           input=inp)
+
+        if p.returncode != 0:
+            raise CombyBinaryError(p.returncode, p.stderr.decode('utf8'))
+
+        out = p.stdout.decode('utf8')
+        logger.debug('raw output: %s', out)
+        return out
+
     def matches(self, source: str, template: str) -> Iterator[Match]:
         logger.info("finding matches of template [%s] in source: %s",
                     template, source)
 
-        cmd = (self.location, '-stdin', '-json-pretty', '-match-only',
-               shlex.quote(template), 'foo')
-        cmd_s = ' '.join(cmd)
+        args = ('-stdin', '-json-pretty', '-match-only',
+                shlex.quote(template), 'foo')
+        args_s = ' '.join(args)
 
-        logger.debug('calling comby: %s', cmd_s)
-        p = subprocess.run(cmd_s,
-                           stderr=subprocess.PIPE,
-                           stdout=subprocess.PIPE,
-                           shell=True,
-                           input=source.encode('utf8'))
-        out = p.stdout.decode('utf8')
-        if p.returncode != 0:
-            raise CombyBinaryError(p.returncode, p.stderr.decode('utf8'))
-
-        jsn = json.loads(out)
+        jsn = json.loads(self.call(args_s, text=source))
         print(jsn)
 
     def rewrite(self,
